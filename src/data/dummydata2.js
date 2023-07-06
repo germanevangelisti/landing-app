2c0a03058403c36032bffb737fb606416c0777f8
German Evangelisti <germanevangelisti60559@gmail.com>
2023-07-06 17:43:12 -0300

    changing dimmydata2 images names not working in amplify hosting

--- src/data/dummydata2.js
+++ src/data/dummydata2.js
@@ -1,35 +1,35 @@
-import image1 from "./../assets/jetsky1.jpeg";
-import image2 from "./../assets/jetsky2.jpeg";
-import image3 from "./../assets/mountainman1.jpeg";
-import image4 from "./../assets/moline1.jpeg";
+import jetsky1 from "./../assets/jetsky1.jpeg";
+import jetsky2 from "./../assets/jetsky2.jpeg";
+import mountainman1 from "./../assets/mountainman1.jpeg";
+import moline1 from "./../assets/moline1.jpeg";
 
 const cards = [
   {
     title: "Experiencias culinarias internacionales",
     description:
       "Sumérgete en una experiencia culinaria única con nuestra selección de platos tradicionales de diferentes países. Descubre sabores y aromas que te transportarán a distintas partes del mundo.",
-    image: image1,
+    image: jetsky1,
   },
   {
     title: "Exploración de senderos naturales",
 
     description:
       "Descubre la belleza natural a través de la exploración de senderos. Recorre parques nacionales, bosques y montañas mientras te conectas con la naturaleza y disfrutas de vistas impresionantes.",
-    image: image2,
+    image: jetsky2,
   },
   {
     title: "Clases de yoga para principiantes",
 
     description:
       "Aprende los fundamentos del yoga en nuestras clases para principiantes. Nuestros instructores certificados te guiarán en una práctica que te ayudará a encontrar equilibrio y bienestar.",
-    image: image3,
+    image: mountainman1,
   },
   {
     title: "Fotografía de paisajes urbanos",
 
     description:
       "Captura la belleza de los paisajes urbanos a través de la fotografía. Aprende técnicas y composición para lograr imágenes impactantes de la arquitectura y vida urbana.",
-    image: image4,
+    image: moline1,
   },
 ];
 
